bc64daf745dcffb6faffde2a952ebdbc58185ecb
kalpesh tita <tita.kalpesh@gmail.com>
2020-07-02 23:25:49 +0530

    created project

---
+++ Jet2Travel/Jet2Travel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jet2Travel
+//
+//  Created by Apple on 02/07/20.
+//  Copyright © 2020 Tita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
